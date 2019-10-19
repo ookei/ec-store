d81147f76face13e26300f55282f98c722040602
OBAYASHI Keisuke <55561870+ookei@users.noreply.github.com>
2019-10-19 12:30:39 +0900

    初期設定

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.browserSync('192.168.10.10:83')
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .version()
